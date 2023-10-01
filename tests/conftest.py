d69cc7d6f862f793e8b6315b3e7f18139c45651e
Anton <anton-fomin89@mail.ru>
2023-10-01 20:07:55 +0300

    1

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope="function", autouse=True)
+def setting_size_browser():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1200
+    browser.config.window_height = 640
+
+
+
